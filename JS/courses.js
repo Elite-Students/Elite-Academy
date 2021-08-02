9e9f5262e9e12fc5107afe486b52877a2e29dd40
asaqa988 <asaqa001@gmail.com>
2021-08-02 16:13:52 +0300

    Update apply.js

--- JS/courses.js
+++ JS/courses.js
@@ -65,7 +65,6 @@ Course.prototype.render = function () {
 for (let i = 0; i < Course.allCourses.length; i++) {
     console.log(Course.allCourses[i])
     Course.allCourses[i].render();
-
 };
 
 function updatecounter() {
